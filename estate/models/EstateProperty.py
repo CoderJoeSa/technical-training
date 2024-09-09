52125dcabd0ba8ca1e0acffaf8ec58587bba83c2
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-09 11:27:05 -0600

    test

--- estate/models/EstateProperty.py
+++ estate/models/EstateProperty.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 class estate_property(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate Property"
 
     name = fields.Char()
