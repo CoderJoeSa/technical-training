e67005a097aaf5ec3ae2c579b813210a2305f82a
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-09 17:58:43 -0600

    add model types properties

--- estate/models/EstatePropertyTipe.py
+++ estate/models/EstatePropertyTipe.py
@@ -5,9 +5,4 @@ class estate_property_type(models.Model):
     _description = "Estate Property Tipe"
 
     name = fields.Char(string="Name", required=True)
-    property_ids = fields.One2many("estate.property", "property_tipe_id", string="Properties")
-    property_count = fields.Integer(string="Property Count", compute="_compute_property_count")
-
-    def _compute_property_count(self):
-        for record in self:
-            record.property_count = len(record.property_ids)
+    
