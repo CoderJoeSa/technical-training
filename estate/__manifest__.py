2316dd8b92c5d6a5a62351ecb16d229766b3d20e
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-09 12:34:00 -0600

    create view on model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,9 +4,11 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
-
+        "data/res.country.state.csv"
     ],
+    "views": [
+        "views/estate_property_views.xml",
+        ],
     "installable": True,
     'license': 'LGPL-3',
 }
