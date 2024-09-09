377f04ce415afbd786fc6ab410df6410cb26c216
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-09 16:46:32 -0600

    update model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
         'security/ir.model.access.csv',
         'views/estate_menu.xml',
         'views/estate_property_views.xml',
+        'views/estate_view.xml',
     ],
     "installable": True,
     'license': 'LGPL-3',
