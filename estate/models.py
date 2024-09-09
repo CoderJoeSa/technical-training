21b7c873dab61e758d5e1c8155ad6c769b8e4822
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-09 10:02:50 -0600

    Create model

--- estate/models.py
+++ estate/models.py
@@ -1,10 +1,2 @@
-from odoo import fields, models
+from odoo import models
 
-class TestModel(models.Model):
-    _name = "test.model"
-    _description = "Test Model"
-    _order = "secuence"
-
-
-    name = fields.Char(string="Name", required=True)  
-    description = fields.Text(string="Description")
---
+++ estate/models/TestModel.py
@@ -0,0 +1,8 @@
+class TestModel(models.Model):
+    _name = "test.model"
+    _description = "Test Model"
+    _order = "secuence"
+
+
+    name = fields.Char(string="Name", required=True)  
+    description = fields.Text(string="Description")
