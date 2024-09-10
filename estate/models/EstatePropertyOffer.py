6273203759ae9ecc3d8b2c393f9c6fac81b4b91c
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-10 13:14:42 -0600

    add model propertyoffer

--- estate/models/EstatePropertyOffer.py
+++ estate/models/EstatePropertyOffer.py
@@ -5,7 +5,7 @@ class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Estate Property Offer"
 
-    property_id = fields.Many2one("estate.property", string="Property", required=True)
+    property_id = fields.One2many("estate.property", string="Property", required=True)
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     price = fields.Float(string="Price", required=True)
     status = fields.Selection(
