114ae78bb8eac8d32ff6f3b49c21608dd575facc
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-10 09:10:34 -0600

    add model types properties

--- estate/models/EstatePropertyTipe.py
+++ estate/models/EstatePropertyTipe.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 class estate_property_type(models.Model):
-    _name = "estate.property.tipe"
+    _name = "estate.property.type"
     _description = "Estate Property Tipe"
 
     name = fields.Char(string="Name", required=True)
