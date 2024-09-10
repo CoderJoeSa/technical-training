93671946bdf3ff4a246d3e60bc8ff19f0eddcded
CoderJoeSa <127345908+CoderJoeSa@users.noreply.github.com>
2024-09-10 11:21:11 -0600

    add

--- estate/models/EstatePropertyOffer.py
+++ estate/models/EstatePropertyOffer.py
@@ -4,7 +4,7 @@ class estate_property_offer (models.Model):
     _name = "estate.property.offer"
     _description = "Estate Property Offer"
 
-    property_id = fields.Many2one("estate.property", string="Property", required=True)
+    # property_id = fields.Many2one("estate.property", string="Property", required=True)
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     price = fields.Float(string="Price", required=True)
     status = fields.Selection(
@@ -12,4 +12,6 @@ class estate_property_offer (models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused'), ('pending', 'Pending')],
         default='pending',
     )
+
+    
    
